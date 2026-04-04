4e5fa6e6b34a4beeac5489f0360186ce28ede03c
Dean1411 <aschendorfdean@gmail.com>
2026-04-04 16:45:55 +0200

    Commit
    Fixed driver issue baseTest class from driver to getDriver();. In the get instance method on extent report class, I put the whole report creation in a if statement to check for null values. I have added a assign category in the onteststart method to get the browers name to be included in the extent report for a single test and parallel testing.

--- src/test/java/reporting/report.java
+++ src/test/java/reporting/report.java
@@ -11,17 +11,18 @@ public class report extends Base {
 	
 	public static ExtentReports getInstance() {
 		
-	    ExtentSparkReporter spark = new ExtentSparkReporter("src/test/resources/reports/extentReport.html");
-	    
-        spark.config().setReportName("Automation Report");
-        spark.config().setDocumentTitle("Test Results");
-        
-	    extent = new ExtentReports();
-	    extent.attachReporter(spark);
-	    
-	    extent.setSystemInfo("Tester", "Dean Aschendorf");
-	    extent.setSystemInfo("Environment", "Development");
-	    extent.setSystemInfo("Browser", Base.getBrowser());
+		if(extent == null) {
+		    ExtentSparkReporter spark = new ExtentSparkReporter("src/test/resources/reports/extentReport.html");
+		    
+	        spark.config().setReportName("Automation Report");
+	        spark.config().setDocumentTitle("Test Results");
+	        
+		    extent = new ExtentReports();
+		    extent.attachReporter(spark);
+		    
+		    extent.setSystemInfo("Tester", "Dean Aschendorf");
+		    extent.setSystemInfo("Environment", "Development");
+		}
 	    
 		return extent;
 	}
